5d502f0849e18d88da3411f4b0f64896060e2334
Tiago Nunes <ntiagon@gmail.com>
2021-08-12 15:50:17 -0300

    db connection and base server

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+
+import express from 'express';
+import mongoose from 'mongoose';
+
+import routes from './routes';
+
+mongoose
+  .connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.emit('db ready');
+  })
+  .catch((e) => console.log(e));
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+app.on('db ready', () => {
+  app.listen(3333),
+    () => {
+      console.log('Connected');
+    };
+});
